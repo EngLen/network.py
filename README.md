655c07db1151f6dbe95a04fdb78f6e2cb47897d1
EngLen <englenfrost@gmail.com>
2019-08-23 12:53:20 +0200

    Update 1.1.0

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # network.py
-A network scanner written by me in python.
+A network scanner written by me in python. UPDATE: v 1.1.0
 commands in tty:
 
 python network.py --t <ip/range>
--- network.py
+++ network.py
@@ -4,28 +4,28 @@ import optparse
 
 
 def get_arguments():
-    parser = optparse.OptionParser()
-    parser.add_option("-t", "--target", dest="target", help="Target IP / IP range.")
-    options, arguments = parser.parse_args()
+    pars = optparse.OptionParser()
+    pars.add_option("-t", "--target", dest="target", help="Target IP / IP range.")
+    options, arguments = pars.parse_args()
     return options
 
 def scan(ip):
-    arp_request = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    clients_list = []
-    for element in answered_list:
-        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
-        clients_list.append(client_dict)
-    return clients_list
+    arp_re = scapy.ARP(pdst=ip)
+    broad_c = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_re_broad_c = broad_c/arp_re
+    anw_ls = scapy.srp(arp_re_broad_c, timeout=1, verbose=False)[0]
+    cl_ls = []
+    for ip in anw_ls:
+        cl_dict = {"ip": ip[1].psrc, "mac": ip[1].hwsrc}
+        cl_ls.append(cl_dict)
+    return cl_ls
 
-def print_result(results_list):
+def print_result(re_ls):
     print("IP\t\t\tMAC Address\n-------------------------------------")
-    for client in results_list:
-        print(client["ip"] + "\t\t" + client["mac"])
+    for cl in re_ls:
+        print(cl["ip"] + "\t\t" + cl["mac"])
 
 
-options = get_arguments()
-scan_result = scan(options.target)
-print_result(scan_result)
+opt = get_arguments()
+scan_re = scan(opt.target)
+print_result(scan_re)
